76803fe39e4adc6378519013fb9e3f92c2c897d1
mjalbert13 <mjalbert13@gmail.com>
2020-03-09 16:46:31 -0400

    added boot strap and dummy data

--- blog_site/blog/views.py
+++ blog_site/blog/views.py
@@ -1,10 +1,34 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+import datetime
 # Create your views here.
 
+posts = [
+    {
+        'author': 'Matt',
+        'title': 'Post 1',
+        'content': "First post",
+        'date': "3/4/17"
+    },
+    {
+        'author': 'Joe',
+        'title': 'Post 2',
+        'content': "hello Worlds",
+        'date': "3/14/19"
+    },
+    {
+        'author': 'Bill',
+        'title': 'Post 3',
+        'content': "Post Up",
+        'date': "3/12/20"
+    },
+
+]
+
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>About Page</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
