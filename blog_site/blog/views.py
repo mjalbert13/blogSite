01e9eba6145fffcc7e15b21729db2cf61963e267
mjalbert13 <mjalbert13@gmail.com>
2020-03-10 14:18:35 -0400

    added db and admin functions

--- blog_site/blog/views.py
+++ blog_site/blog/views.py
@@ -1,32 +1,12 @@
 from django.shortcuts import render
 import datetime
+from .models import Post
 # Create your views here.
 
-posts = [
-    {
-        'author': 'Matt',
-        'title': 'Post 1',
-        'content': "First post",
-        'date': "3/4/17"
-    },
-    {
-        'author': 'Joe',
-        'title': 'Post 2',
-        'content': "hello Worlds",
-        'date': "3/14/19"
-    },
-    {
-        'author': 'Bill',
-        'title': 'Post 3',
-        'content': "Post Up",
-        'date': "3/12/20"
-    },
-
-]
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
